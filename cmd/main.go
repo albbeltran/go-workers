698b9d6e5c565ef6baa3e3b8312db3b62aacd1cd
Alberto Beltran <albertobeltran@protonmail.com>
2025-11-05 21:29:35 -0600

    Defined worker structure

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func main() {
-	fmt.Println("Hello, World!")
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for job := range jobs {
+		fmt.Printf("El worker %d inicio la tarea %d\n", id, job)
+		time.Sleep(300 * time.Millisecond)
+
+		result := job
+
+		results <- result
+
+		fmt.Printf("El worker %d finalizo la tarea %d\n", id, job)
+	}
 }
