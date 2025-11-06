7c0f8458ace5c68b801a89a64f57136730617132
Alberto Beltran <albertobeltran@protonmail.com>
2025-11-05 23:42:04 -0600

    Added main function to init worker pool and print results

--- cmd/main.go
+++ cmd/main.go
@@ -17,3 +17,26 @@ func worker(id int, jobs <-chan int, results chan<- int) {
 		fmt.Printf("El worker %d finalizo la tarea %d\n", id, job)
 	}
 }
+
+func main() {
+	const numWorkers = 4
+	const numJobs = 20
+
+	jobs := make(chan int, numJobs)
+	results := make(chan int, numJobs)
+
+	for w := 1; w <= numWorkers; w++ {
+		go worker(w, jobs, results)
+	}
+
+	go func() {
+		for j := 1; j <= numJobs; j++ {
+			jobs <- j
+		}
+		close(jobs)
+	}()
+
+	for r := 1; r <= numJobs; r++ {
+		<-results
+	}
+}
